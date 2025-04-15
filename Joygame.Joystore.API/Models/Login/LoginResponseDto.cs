af5d0133aab583f4c7e4f6846d14280b4f53af97
berkay.baskaya <bbaskaya@payby.me>
2025-04-15 23:49:38 +0300

    feat: Add Login Dtos

--- Joygame.Joystore.API/Models/Login/LoginResponseDto.cs
+++ Joygame.Joystore.API/Models/Login/LoginResponseDto.cs
@@ -2,5 +2,20 @@
 {
     public class LoginResponseDto
     {
+        public TokenDto Token { get; set; }
+        public UserDto User { get; set; }
+
+    }
+    public class TokenDto
+    {
+        public string AccessToken { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public List<string> Roles { get; set; }
     }
 }
