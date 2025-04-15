7b5687651ac625eb3cb4df3ab675aeed9048511f
berkay.baskaya <bbaskaya@payby.me>
2025-04-15 23:49:53 +0300

    feat: Add Token provider

---
+++ Joygame.Joystore.API/Security/ITokenProvider.cs
@@ -0,0 +1,11 @@
+﻿using Joygame.Joystore.API.Models.Login;
+using System.Security.Claims;
+
+namespace Joygame.Joystore.API.Security
+{
+    public interface ITokenProvider
+    {
+        TokenDto GenerateToken(IEnumerable<Claim> claims);
+
+    }
+}
---
+++ Joygame.Joystore.API/Security/TokenProvider.cs
@@ -0,0 +1,41 @@
+﻿using Joygame.Joystore.API.Models.Login;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Joygame.Joystore.API.Security
+{
+    public class TokenProvider : ITokenProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public TokenDto GenerateToken(IEnumerable<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiresAt = DateTime.UtcNow.AddDays(3);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: expiresAt,
+                signingCredentials: credentials
+            );
+
+            return new TokenDto
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiresAt
+            };
+        }
+
+    }
+}
