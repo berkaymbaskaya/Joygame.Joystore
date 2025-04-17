0757f9e80efad63230257a521f9f73e3cdaa7e16
berkay.baskaya <bbaskaya@payby.me>
2025-04-17 10:14:07 +0300

    feat: Add GetRecursiveCategories.sql

---
+++ Joygame.Joystore.API/Scripts/sp_GetRecursiveCategories.sql
@@ -0,0 +1,30 @@
+﻿CREATE OR ALTER PROCEDURE sp_GetRecursiveCategories
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    WITH RecursiveCategory AS
+    (
+        SELECT 
+            Id,
+            Name,
+            ParentId
+        FROM Categories
+        WHERE ParentId IS NULL AND IsActive = 1 AND IsDeleted = 0
+
+        UNION ALL
+
+        SELECT 
+            c.Id,
+            c.Name,
+            c.ParentId
+        FROM Categories c
+        INNER JOIN RecursiveCategory rc ON c.ParentId = rc.Id
+        WHERE c.IsActive = 1 AND c.IsDeleted = 0
+    )
+    SELECT 
+        Id,
+        Name,
+        ParentId
+    FROM RecursiveCategory
+END
