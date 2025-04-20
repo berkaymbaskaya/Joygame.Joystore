4de6d4e1a23ff3434669178463ae169d504ee2b3
berkay.baskaya <bbaskaya@payby.me>
2025-04-20 22:46:30 +0300

    feat: Add product service

---
+++ Joygame.Joystore.Services/Interfaces/IProductSerive.cs
@@ -0,0 +1,18 @@
+﻿using Joygame.Joystore.API.Models.Login;
+using Joygame.Joystore.API.Models.Product;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joygame.Joystore.Services.Interfaces
+{
+    public interface IProductSerive
+    {
+        [Get("/Product")]
+        Task<API.Core.PagedResult<ProductViewDto>> Product(int pageNumber,int pageSize);
+
+    }
+}
